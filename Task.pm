4e25a3c18d9eb775f620210c4d35deb13566bc97
Abram Hindle <abram.hindle@ualberta.ca>
2012-09-28 13:45:21 -0400

    Fix Task Moose Syntax (pass another test)

--- Task.pm
+++ Task.pm
@@ -2,13 +2,13 @@ package Task;
 use Moose;
 
 # unique identifier
-has 'id' => (is => 'rw', isa => 'String');
+has 'id' => (is => 'rw', isa => 'Str');
 # one line summary
-has 'summary' => ( is => 'rw', isa => 'String');
+has 'summary' => ( is => 'rw', isa => 'Str');
 # long form description
-has 'description' => (is => 'rw', isa => 'String');
+has 'description' => (is => 'rw', isa => 'Str');
 # content will be free text like json
-has 'content' => ( is => 'rw', isa => 'String');
+has 'content' => ( is => 'rw', isa => 'Str');
 
 
 sub hashify {
